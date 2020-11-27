5bca3208f55e2405eeb2eee7a569b7957bc45b39
nikhilbadam56 <122004173@sastra.ac.in>
2020-11-27 11:41:57 +0530

    Headers were added for functionality

--- app/src/main/java/com/example/triveousassignment/nbc_news_fragment.java
+++ app/src/main/java/com/example/triveousassignment/nbc_news_fragment.java
@@ -117,6 +117,7 @@ public class nbc_news_fragment extends Fragment implements Article_adapter.Artic
                 URL myUrl = new URL(stringUrl);
                 HttpURLConnection connection =(HttpURLConnection)myUrl.openConnection();
                 connection.setRequestMethod("GET");
+                connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
                 Log.i("background:","Task1 Completed");
                 connection.connect();
                 InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
